385b34844fd5a8d6fd652c633909ef24ffa60fa8
Yashasvi <66861659+stlyash@users.noreply.github.com>
2024-02-11 08:35:21 +0000

    Update compProg.sh

--- compProg.sh
+++ compProg.sh
@@ -5,6 +5,9 @@ sudo apt upgrade
 # installing g++ to run C++
 sudo apt install g++
 
+# Uncomment the below file to install python
+# sudo apt get python
+
 # installing vscode
 sudo dpkg -i "vscode.deb"
 
