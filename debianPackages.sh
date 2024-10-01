32cd1b000daa0d9def5ad0741afece55dfb47ee9
Yashasvi <kmryashasvi@gmail.com>
2024-10-01 22:25:57 +0530

    made download and delete scripts

---
+++ debianPackages.sh
@@ -0,0 +1,6 @@
+su
+touch prepareDebian.sh
+chmod 777 prepareDebian.sh
+(curl https://raw.githubusercontent.com/yashlikescode/prepareLinux/refs/heads/main/debianDownloadScripts.sh) > prepareDebian.sh
+bash ./prepareDebian.sh
+rm prepareDebian.sh
