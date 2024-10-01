ff1fd26df7ce0401d2158123fe64a22528a27298
Yashasvi <kmryashasvi@gmail.com>
2024-10-01 22:32:24 +0530

    modified master package

--- debianPackages.sh
+++ debianPackages.sh
@@ -1,6 +1,5 @@
-su
-touch prepareDebian.sh
-chmod 777 prepareDebian.sh
-(curl https://raw.githubusercontent.com/yashlikescode/prepareLinux/refs/heads/main/debianDownloadScripts.sh) > prepareDebian.sh
-bash ./prepareDebian.sh
-rm prepareDebian.sh
+sudo touch prepareDebian.sh
+sudo chmod 777 prepareDebian.sh
+sudo curl https://raw.githubusercontent.com/yashlikescode/prepareLinux/refs/heads/main/debianDownloadScripts.sh -o prepareDebian.sh
+sudo bash ./prepareDebian.sh
+sudo rm prepareDebian.sh
